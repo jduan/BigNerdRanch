38c7113af91529fd738923978b2c2e1081d17846
Jingjing Duan <duanjingjing@gmail.com>
2018-04-16 22:00:15 -0700

    Protocols

--- BigNerdRanch/main.swift
+++ BigNerdRanch/main.swift
@@ -130,3 +130,99 @@ gods.last?.name = "Jupiter"
 // Both gods and godsCopy's last element have been changed!
 print("gods: \(gods.last?.name)")
 print("godsCopy: \(godsCopy.last?.name)")
+
+/////////////////////////////////////////////////////////////////////////////
+// protocols
+/////////////////////////////////////////////////////////////////////////////
+
+struct Person {
+    let name: String
+    let age: Int
+    let yearsOfExperience: Int
+}
+
+protocol TabularDataSource {
+    // for read-write properties, use "{ get set }"
+    var numberOfRows: Int { get }
+    var numberOfColumns: Int { get }
+
+    func label(forColumn column: Int) -> String
+    func itemFor(row: Int, column: Int) -> String
+}
+
+// implements a Protocol
+struct Department: TabularDataSource {
+    let name: String
+    var people = [Person]()
+
+    init(name: String) {
+        self.name = name
+    }
+
+    mutating func add(_ person: Person) {
+        people.append(person)
+    }
+
+    var numberOfRows: Int {
+        return people.count
+    }
+
+    var numberOfColumns: Int {
+        return 3
+    }
+
+    func label(forColumn column: Int) -> String {
+        switch column {
+        case 0: return "Employee Name"
+        case 1: return "Age"
+        case 2: return "Years of Experience"
+        default: fatalError("Invalid column!")
+        }
+    }
+
+    func itemFor(row: Int, column: Int) -> String {
+        let person = people[row]
+        switch column {
+        case 0: return person.name
+        case 1: return String(person.age)
+        case 2: return String(person.yearsOfExperience)
+        default: fatalError("Invalid column!")
+        }
+    }
+}
+
+var department = Department(name: "Engineering")
+department.add(Person(name: "Joe", age: 30, yearsOfExperience: 6))
+department.add(Person(name: "Karen", age: 40, yearsOfExperience: 18))
+department.add(Person(name: "Fred", age: 50, yearsOfExperience: 20))
+
+// use a Protocol in a function
+func printTable(_ dataSource: TabularDataSource) {
+    // keep track of the width of each column
+    var columnWidths = [Int]()
+
+    // Create first row containing column headers
+    var firstRow = "|"
+    var i = 0
+    while i < dataSource.numberOfColumns {
+        let columnLabel = dataSource.label(forColumn: i)
+        let columnHeader = " \(columnLabel) |"
+        firstRow += columnHeader
+        columnWidths.append(columnLabel.count)
+        i += 1
+    }
+    print(firstRow)
+
+    for i in 0 ..< dataSource.numberOfRows {
+        var out = "|"
+        for j in 0 ..< dataSource.numberOfColumns {
+            let item = dataSource.itemFor(row: i, column: j)
+            let paddingNeeded = columnWidths[j] - item.count
+            let padding = repeatElement(" ", count: paddingNeeded).joined(separator: "")
+            out += " \(padding)\(item) |"
+        }
+        print(out)
+    }
+}
+
+printTable(department)
